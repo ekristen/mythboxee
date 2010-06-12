de722ecde70f7606d8427a6772010b666ab5898d
Erik Kristensen <erik@erikkristensen.com>
2010-06-12 15:55:33 -0400

    fixed launch.py so that server is requested until verified

--- mythboxee/launch.py
+++ mythboxee/launch.py
@@ -2,28 +2,41 @@ import mc
 config = mc.GetApp().GetLocalConfig()
 http = mc.Http()
 
+#Debug Code
+#config.SetValue("verified", "")
+#config.SetValue("server", "")
+
+server = config.GetValue("server")
+verified = config.GetValue("verified")
+
 def GetServer():
 		config = mc.GetApp().GetLocalConfig()
 		server = config.GetValue("server")
-		response = mc.ShowDialogKeyboard("Enter Full URL to Boxee Script (ie http://192.168.1.210/boxee.php)", server, False)
+		response = mc.ShowDialogKeyboard("Enter Full URL to MythBoxee Script", server, False)
 		if not response and not server:
 			GetServer()
 		elif response:
 			config.SetValue("server", response)
 
-# Check to see if the server variable has been set
-# if it has not then ask for input from user
-if not server:
-	GetServer()
 
-# Verify that the script has been installed at the location
-# specifed by the user
-data = http.Get(config.GetValue("server") + "?type=verify")
-if http.GetHttpResponseCode() != 200:
-	mc.ShowDialogOk("Error", "Unable to connect MythBoxee Script on MythBackend")
-	GetServer()
+def VerifyServer():
+		data = http.Get(config.GetValue("server") + "?type=verify")
+		if http.GetHttpResponseCode() != 200:
+			mc.ShowDialogOk("Error", "Unable to connect MythBoxee Script on MythBackend")
+			GetServer()
+			VerifyServer()
+		else:
+			config.SetValue("verified", "1")
+
+while not server and not verified:
+	if not server:
+		GetServer()
+		server = config.GetValue("server")
+	if not verified:
+		VerifyServer()
+		verified = config.GetValue("verified")
 
 # If server is set, and at this point it has been verified
 # then launch the application
-if server:	
+if verified:
 	mc.ActivateWindow(14000)
