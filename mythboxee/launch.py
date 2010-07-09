2a9d2a8bb5bc0f8b2de1c90431263bef022b8746
Erik Kristensen <erik@erikkristensen.com>
2010-07-09 18:53:11 -0400

    now on version 3.0.beta

--- mythboxee/launch.py
+++ mythboxee/launch.py
@@ -1,35 +1,27 @@
 import mc
+import mythboxee
+
+# Get Access to the Apps Local Config
 config = mc.GetApp().GetLocalConfig()
-http = mc.Http()
 
 # For Debugging
 #config.SetValue("verified", "")
 #config.SetValue("server", "")
 
+# Pull out some of the variables we need
 server = config.GetValue("server")
 verified = config.GetValue("verified")
 
-def GetServer():
-		config = mc.GetApp().GetLocalConfig()
-		server = config.GetValue("server")
-		response = mc.ShowDialogKeyboard("Enter Full URL to MythBoxee Script", server, False)
-		if VerifyServer(response) == True:
-			config.SetValue("server", response)
-
-def VerifyServer(url):
-		data = http.Get(url + "?type=verify")
-		if http.GetHttpResponseCode() == 200:
-			config.SetValue("verified", "1")
-			return True
-		else:
-			return False
-
+# If the server hasn't been defined, we need to get it.
 if not server:
-	GetServer()
+	mythboxee.GetServer()
 
 # If server is set, and at this point it has been verified
 # then launch the application
 if config.GetValue("verified") == "1":
 	mc.ActivateWindow(14000)
+
+	# Load all the show data from the MythTV Backend Server
+	mythboxee.LoadShows()
 else:
 	mc.ShowDialogOk("MythBoxee Error", "You must enter the full path to the MythBoxee script or MythBoxee was unable to verify the URL provided.")
